729527cb55a277755e9ee2d6571a449ef8229efa
Sitasafe <willysitasafe@gmail.com>
2026-03-11 20:24:09 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -382,7 +382,7 @@ with tab_ins:
 
 # --- TAB 3: EQUIPO ---
 with tab_team:
-    st.markdown("### 👥 Nuestro Equipo Multidisciplinario")
+    st.markdown("### 👥 Nuestro Equipo")
     equipo = [
         ("Willan Álvarez.", "Lead Architect", "https://i.imgur.com/CSH9Af7.jpeg"),
         ("Dalia R.", "Product Manager", "https://i.imgur.com/4O2BGL8.jpeg"),
@@ -408,5 +408,6 @@ with tab_team:
 st.markdown('</div>', unsafe_allow_html=True) 
 st.write("---")
 st.caption("Growth Copilot | Equipo 3 | Hackathon UTEL 2026 | TiendaNube")
+
 
 
